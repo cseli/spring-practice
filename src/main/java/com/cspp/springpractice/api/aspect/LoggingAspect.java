d7a4ff2b4b0a770bc15cee4d5e8c39f758c0b4e8
Peter Cselenko <Peter.Cselenko@logmein.com>
2018-02-24 16:59:31 +0100

    add slf4j logger

--- src/main/java/com/cspp/springpractice/api/aspect/LoggingAspect.java
+++ src/main/java/com/cspp/springpractice/api/aspect/LoggingAspect.java
@@ -5,17 +5,20 @@ import java.util.Arrays;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 @Aspect
 public class LoggingAspect {
+	private static final Logger LOGGER = LoggerFactory.getLogger(LoggingAspect.class);
 
 	@Before("execution(* com.cspp.springpractice.api.car.CarController.*(..))")
 	public void logCarController(JoinPoint joinPoint) {
-		System.out.println("*** " + joinPoint.getSignature().getName() + " has been called, with parameter:");
+		LOGGER.info("*** " + joinPoint.getSignature().getName() + " has been called, with parameter:");
 		Object[] objects = joinPoint.getArgs();
-		Arrays.stream(objects).forEach(System.out::println);
+		Arrays.stream(objects).forEach(object -> LOGGER.info(object.toString()));
 	}
 
 }
