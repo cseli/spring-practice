82907b60ac3506750e5ca1223d45ffd47413ccf9
Peter Cselenko <Peter.Cselenko@logmein.com>
2018-02-24 16:11:43 +0100

    add logging aspect

--- src/main/java/com/cspp/springpractice/api/aspect/LoggingAspect.java
+++ src/main/java/com/cspp/springpractice/api/aspect/LoggingAspect.java
@@ -1,7 +1,21 @@
 package com.cspp.springpractice.api.aspect;
 
+import java.util.Arrays;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 @Component
+@Aspect
 public class LoggingAspect {
+
+	@Before("execution(* com.cspp.springpractice.api.car.CarController.*(..))")
+	public void logCarController(JoinPoint joinPoint) {
+		System.out.println("*** " + joinPoint.getSignature().getName() + " has been called, with parameter:");
+		Object[] objects = joinPoint.getArgs();
+		Arrays.stream(objects).forEach(System.out::println);
+	}
+
 }
